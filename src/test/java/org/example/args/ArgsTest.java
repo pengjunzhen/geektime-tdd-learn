6d54eea7b964fc28106f467de2f47fb37df94241
jun <jun.peng1@tec-do.com>
2023-04-16 13:58:39 +0800

    add: 构思解析参数程序对外的使用方式-数组

--- src/test/java/org/example/args/ArgsTest.java
+++ src/test/java/org/example/args/ArgsTest.java
@@ -2,6 +2,10 @@ package org.example.args;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ArgsTest {
 
     // 策略一：用下标去直接定位
@@ -22,13 +26,23 @@ public class ArgsTest {
     // {-l:[], -p:[8080], -d:[/usr/logs] }
 
     @Test
-    public void should() {
+    public void should_example_1() {
         Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        options.logging();
-        options.port();
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+    }
+
+    @Test
+    public void should_example_2() {
+        ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
+        assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
+        assertArrayEquals(new int[]{1, 2, -3, 5}, options.decimals());
     }
 
     record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
+
+    record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
     }
 
 }
