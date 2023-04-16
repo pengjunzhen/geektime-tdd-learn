2de675ce39c9cff375210392fb41215f1c251a83
jun <jun.peng1@tec-do.com>
2023-04-16 14:41:36 +0800

    add: bool类型测试用例：存在-绿

--- src/main/java/org/example/args/Args.java
+++ src/main/java/org/example/args/Args.java
@@ -1,8 +1,17 @@
 package org.example.args;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Args {
 
     public static <T> T parse(Class<T> optionsClass, String... args) {
-        return null;
+        Constructor<?> declaredConstructor = optionsClass.getDeclaredConstructors()[0];
+        try {
+            return (T) declaredConstructor.newInstance(true);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
