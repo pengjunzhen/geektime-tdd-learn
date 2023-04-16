8a9bb3e2549b3988ae7753f0fc571f884a7715b0
jun <jun.peng1@tec-do.com>
2023-04-16 15:13:32 +0800

    add: multi option测试用例-红

--- src/test/java/org/example/args/ArgsTest.java
+++ src/test/java/org/example/args/ArgsTest.java
@@ -70,6 +70,17 @@ public class ArgsTest {
 
     // Multi options:
     // TODO:  -l -p 8080 -d /usr/logs
+    @Test
+    public void should_parse_multi_options() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
+    }
+
+    record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
+
     // Sad Path:
     // TODO: - bool: -l t / -l t f
     // TODO: - int: -p / -p 8080 9090
@@ -81,21 +92,10 @@ public class ArgsTest {
 
     @Test
     @Disabled
-    public void should_example_1() {
-        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging());
-        assertEquals(8080, options.port());
-    }
-
-    @Test
-    @Disabled
     public void should_example_2() {
         ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
         assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
         assertArrayEquals(new int[]{1, 2, -3, 5}, options.decimals());
-    }
-
-    record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
     }
 
     record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
