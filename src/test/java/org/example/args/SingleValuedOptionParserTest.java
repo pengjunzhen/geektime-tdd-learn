3883e76293c85c4776b9fc4e393b4ad77fb1ee1b
jun <pengjunzhenwork@163.com>
2023-09-03 16:02:21 +0800

    refactor: SingleValuedOptionParserTest测试happy path,default path 更通用

--- src/test/java/org/example/args/SingleValuedOptionParserTest.java
+++ src/test/java/org/example/args/SingleValuedOptionParserTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Arrays;
+import java.util.function.Function;
 
 import static java.util.Arrays.asList;
 import static org.example.args.BooleanOptionParserTest.option;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SingleValuedOptionParserTest {
@@ -33,13 +34,6 @@ public class SingleValuedOptionParserTest {
         assertEquals("p", e.getOption());
     }
 
-    // default path
-    @Test
-    public void should_set_default_value_to_0_for_int_option() {
-
-        assertEquals(0, new SingleValueOptionParser<>(0, Integer::parseInt).parse(asList(), option("p")));
-    }
-
     @Test
     public void should_not_accept_extra_argument_for_string_value_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
@@ -49,9 +43,22 @@ public class SingleValuedOptionParserTest {
         assertEquals("d", e.getOption());
     }
 
+    // default path
+    @Test
+    public void should_set_default_value_for_single_value_option() {
+        Function<String, Object> whatever = (it) -> null;
+        Object defaultValue = new Object();
+
+        assertSame(defaultValue, new SingleValueOptionParser<>(defaultValue, whatever).parse(asList(), option("p")));
+    }
+
     // happy path
     @Test
     public void should_parse_value_if_flag_present() {
-        assertEquals(8080, new SingleValueOptionParser<>(0, Integer::parseInt).parse(asList("-p", "8080"), option("p")));
+        Object parsed = new Object();
+        Function<String, Object> parse = (it) -> parsed;
+        Object whatever = new Object();
+
+        assertSame(parsed, new SingleValueOptionParser<>(whatever, parse).parse(asList("-p", "8080"), option("p")));
     }
 }
