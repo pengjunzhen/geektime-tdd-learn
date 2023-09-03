8de67a84a40f1b42427d4ca5489d2923936e9dcc
jun <pengjunzhenwork@163.com>
2023-09-03 12:45:28 +0800

    feat: 利用多态替换条件分支-提取变量，寻找共同点

--- src/main/java/org/example/args/Args.java
+++ src/main/java/org/example/args/Args.java
@@ -24,13 +24,16 @@ public class Args {
         Object value = null;
         Option option = parameter.getAnnotation(Option.class);
         if (parameter.getType() == boolean.class) {
-            value = new BooleanOptionParser().parse(arguments, option);
+            BooleanOptionParser parser = new BooleanOptionParser();
+            value = parser.parse(arguments, option);
         }
         if (parameter.getType() == int.class) {
-            value = new IntOptionParser().parse(arguments, option);
+            IntOptionParser parser = new IntOptionParser();
+            value = parser.parse(arguments, option);
         }
         if (parameter.getType() == String.class) {
-            value = new StringOptionParser().parse(arguments, option);
+            StringOptionParser parser = new StringOptionParser();
+            value = parser.parse(arguments, option);
         }
         return value;
     }
