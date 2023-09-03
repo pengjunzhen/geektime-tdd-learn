41ff38d238414741bc13632ea01141338dcd05bc
jun <pengjunzhenwork@163.com>
2023-09-03 13:38:23 +0800

    Refactor: 消除重复-StringOptionParser改用构造函数创建;移除无用方法

--- src/main/java/org/example/args/StringOptionParser.java
+++ src/main/java/org/example/args/StringOptionParser.java
@@ -2,8 +2,7 @@ package org.example.args;
 
 class StringOptionParser extends IntOptionParser {
 
-    @Override
-    protected Object parseValue(String value) {
-        return String.valueOf(value);
+    public StringOptionParser() {
+        super(String::valueOf);
     }
 }
