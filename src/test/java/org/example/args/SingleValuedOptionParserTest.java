9ede764977cba5bff887e017825c9f9b8345b711
jun <pengjunzhenwork@163.com>
2023-09-03 15:34:39 +0800

    refactor: 测试string多参数报错的场景

--- src/test/java/org/example/args/SingleValuedOptionParserTest.java
+++ src/test/java/org/example/args/SingleValuedOptionParserTest.java
@@ -33,6 +33,15 @@ public class SingleValuedOptionParserTest {
     @Test
     public void should_set_default_value_to_0_for_int_option() {
 
-        assertEquals(0, new SingleValueOptionParser<Integer>(0, Integer::parseInt).parse(asList(), option("p")));
+        assertEquals(0, new SingleValueOptionParser<>(0, Integer::parseInt).parse(asList(), option("p")));
+    }
+
+    @Test
+    public void should_not_accept_extra_argument_for_string_value_option() {
+        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
+            new SingleValueOptionParser<>(0, String::valueOf).parse(asList("-d", "/usr/logs", "/usr/vars"), option("d"));
+        });
+
+        assertEquals("d", e.getOption());
     }
 }
