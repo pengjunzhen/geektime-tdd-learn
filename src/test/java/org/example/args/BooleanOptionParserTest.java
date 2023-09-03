f77fa4ad6af9ef288d64806722b89630996a9116
jun <pengjunzhenwork@163.com>
2023-09-03 15:41:10 +0800

    feat: 测试boolean默认值-增加正确情况的测试

--- src/test/java/org/example/args/BooleanOptionParserTest.java
+++ src/test/java/org/example/args/BooleanOptionParserTest.java
@@ -5,23 +5,30 @@ import org.junit.jupiter.api.Test;
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BooleanOptionParserTest {
 
     @Test
     public void should_not_accept_extra_argument_for_boolean_option() {
         TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class,
-                () -> new BooleanOptionParser()
-                        .parse(Arrays.asList("-l", "t"), option("l")));
+                () -> new BooleanOptionParser<>()
+                        .parse(asList("-l", "t"), option("l")));
         assertEquals("l", e.getOption());
     }
 
     @Test
     public void should_set_default_value_to_false_if_option_not_present() {
-        assertFalse(new BooleanOptionParser().parse(Arrays.asList(), option("l")));
+        assertFalse(new BooleanOptionParser<>().parse(asList(), option("l")));
+    }
+
+    @Test
+    public void should_set_default_value_to_true_if_option_present() {
+        assertTrue(new BooleanOptionParser<>().parse(asList("-l"), option("l")));
     }
 
     static Option option(String value) {
