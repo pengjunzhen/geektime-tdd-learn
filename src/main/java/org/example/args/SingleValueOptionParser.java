7c7df57d7937410867abfdc60f0b64f92a41629e
jun <pengjunzhenwork@163.com>
2023-09-03 15:23:15 +0800

    feat: 测试int默认值-SingleValueOptionParser增加默认值

--- src/main/java/org/example/args/SingleValueOptionParser.java
+++ src/main/java/org/example/args/SingleValueOptionParser.java
@@ -5,15 +5,24 @@ import java.util.function.Function;
 
 class SingleValueOptionParser<T> implements OptionParser<T> {
 
+    private T defaultValue;
     private Function<String, T> valueParser;
 
     public SingleValueOptionParser(Function<String, T> valueParser) {
         this.valueParser = valueParser;
     }
 
+    public SingleValueOptionParser(T defaultValue, Function<String, T> valueParser) {
+        this.defaultValue = defaultValue;
+        this.valueParser = valueParser;
+    }
+
     @Override
     public T parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
+        if (index == -1) {
+            return defaultValue;
+        }
         if (index + 1 == arguments.size()
                 || arguments.get(index + 1).startsWith("-")) {
             throw new InsufficientArgumentsException(option.value());
