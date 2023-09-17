40423edf100ce096a4b26ae13d44d6167f5166ad
jun <pengjunzhenwork@163.com>
2023-09-17 22:01:37 +0800

    refactor: 利用optional处理条件分支

---
+++ src/main/java/org/example/args/IllegalValueException.java
@@ -0,0 +1,29 @@
+package org.example.args;
+
+public class IllegalValueException extends RuntimeException {
+
+    private String option;
+
+    private String value;
+
+    public IllegalValueException(String option, String value) {
+        this.option = option;
+        this.value = value;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- src/main/java/org/example/args/SingleValueOptionParser.java
+++ src/main/java/org/example/args/SingleValueOptionParser.java
@@ -1,6 +1,7 @@
 package org.example.args;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
@@ -16,22 +17,33 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
 
     @Override
     public T parse(List<String> arguments, Option option) {
+        Optional<List<String>> argumentList;
+
         int index = arguments.indexOf("-" + option.value());
         if (index == -1) {
-            return defaultValue;
-        }
+            argumentList = Optional.empty();
+        } else {
 
-        List<String> values = values(arguments, index);
+            List<String> values = values(arguments, index);
 
-        if (values.size() < 1) {
-            throw new InsufficientArgumentsException(option.value());
-        }
+            if (values.size() < 1) {
+                throw new InsufficientArgumentsException(option.value());
+            }
 
-        if (values.size() > 1) {
-            throw new TooManyArgumentsException(option.value());
+            if (values.size() > 1) {
+                throw new TooManyArgumentsException(option.value());
+            }
+            argumentList = Optional.of(values);
         }
-        String value = values.get(0);
-        return valueParser.apply(value);
+        return argumentList.map(it -> parseValue(option, it.get(0))).orElse(defaultValue);
+    }
+
+    private T parseValue(Option option, String value) {
+        try {
+            return valueParser.apply(value);
+        } catch (Exception e) {
+            throw new IllegalValueException(option.value(), value);
+        }
     }
 
     static List<String> values(List<String> arguments, int index) {
