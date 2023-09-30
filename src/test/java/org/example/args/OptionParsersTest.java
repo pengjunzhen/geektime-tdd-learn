dad128a82c8303c4a97b158fca02b74969dc9327
jun <pengjunzhenwork@163.com>
2023-09-30 21:15:39 +0800

    refactor: 统一风格 -> 调整测试

---
+++ src/test/java/org/example/args/OptionParsersTest.java
@@ -0,0 +1,114 @@
+package org.example.args;
+
+import org.example.args.exceptions.InsufficientArgumentsException;
+import org.example.args.exceptions.TooManyArgumentsException;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.annotation.Annotation;
+import java.util.function.Function;
+
+import static java.util.Arrays.asList;
+import static org.example.args.OptionParsersTest.BooleanOptionParser.option;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OptionParsersTest {
+
+    @Nested
+    class UnaryOptionParser {
+
+        @Test
+        public void should_not_accept_extra_argument_for_single_value_option() {
+            TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
+                OptionParsers.unary(0, Integer::parseInt).parse(asList("-p", "8080", "8080"), option("p"));
+            });
+
+            assertEquals("p", e.getOption());
+        }
+
+        // sad path
+        @ParameterizedTest
+        @ValueSource(strings = {"-p -l", "-p"})
+        public void should_not_accept_insufficient_argument_for_single_value_option(String arguments) {
+            InsufficientArgumentsException e = assertThrows(InsufficientArgumentsException.class, () -> {
+                OptionParsers.unary(0, Integer::parseInt).parse(asList(arguments.split(" ")), option("p"));
+            });
+
+            assertEquals("p", e.getOption());
+        }
+
+        @Test
+        public void should_not_accept_extra_argument_for_string_value_option() {
+            TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
+                OptionParsers.unary(0, String::valueOf).parse(asList("-d", "/usr/logs", "/usr/vars"), option("d"));
+            });
+
+            assertEquals("d", e.getOption());
+        }
+
+        // default path
+        @Test
+        public void should_set_default_value_for_single_value_option() {
+            Function<String, Object> whatever = (it) -> null;
+            Object defaultValue = new Object();
+
+            assertSame(defaultValue, OptionParsers.unary(defaultValue, whatever).parse(asList(), option("p")));
+        }
+
+        // happy path
+        @Test
+        public void should_parse_value_if_flag_present() {
+            Object parsed = new Object();
+            Function<String, Object> parse = (it) -> parsed;
+            Object whatever = new Object();
+
+            assertSame(parsed, OptionParsers.unary(whatever, parse).parse(asList("-p", "8080"), option("p")));
+        }
+    }
+
+    @Nested
+    class BooleanOptionParser {
+
+        // sad path
+        @Test
+        public void should_not_accept_extra_argument_for_boolean_option() {
+            TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class,
+                    () -> OptionParsers.bool()
+                            .parse(asList("-l", "t"), option("l")));
+            assertEquals("l", e.getOption());
+        }
+
+        // default path
+        @Test
+        public void should_set_default_value_to_false_if_option_not_present() {
+            assertFalse(OptionParsers.bool().parse(asList(), option("l")));
+        }
+
+        // happy path
+        @Test
+        public void should_set_default_value_to_true_if_option_present() {
+            assertTrue(OptionParsers.bool().parse(asList("-l"), option("l")));
+        }
+
+        static Option option(String value) {
+            return new Option() {
+
+                @Override
+                public Class<? extends Annotation> annotationType() {
+                    return Option.class;
+                }
+
+                @Override
+                public String value() {
+                    return value;
+                }
+            };
+        }
+    }
+}
